a9ba2782c69571544d53b0ded5466cd8eddc6738
Xin Zhang <jason@Xins-MacBook-Pro.local>
2023-03-07 22:40:36 -0800

    add tables.sql

--- tables.sql
+++ tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 03, 2023 at 11:28 PM
+-- Generation Time: Mar 08, 2023 at 07:39 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -29,15 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `event` (
   `idEvent` int(11) NOT NULL,
-  `hashIdEvent` varchar(16) NOT NULL,
-  `hashUsers` varchar(255) NOT NULL,
+  `hashEvent` varchar(8) DEFAULT NULL,
+  `hashUsers` varchar(8) DEFAULT NULL,
   `topic` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
   `method` varchar(16) DEFAULT NULL,
-  `allowComment` varchar(16) DEFAULT NULL,
-  `allowUpload` varchar(16) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
+  `allowComment` int(4) DEFAULT NULL,
+  `allowUpload` int(4) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `origFileName` varchar(255) DEFAULT NULL,
+  `storedFileName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`idEvent`, `hashEvent`, `hashUsers`, `topic`, `location`, `method`, `allowComment`, `allowUpload`, `description`, `origFileName`, `storedFileName`) VALUES
+(41, '3416a75f', '1d88b7b7', 'week 8 meeting', 'zoom.com', 'virtual', 0, 0, '\n', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -47,11 +56,23 @@ CREATE TABLE `event` (
 
 CREATE TABLE `options` (
   `idOptions` int(11) NOT NULL,
-  `idEvent` int(11) NOT NULL,
+  `idEvent` int(11) DEFAULT NULL,
   `duration` varchar(16) DEFAULT NULL,
   `date` timestamp NULL DEFAULT NULL,
-  `timeslots` timestamp NULL DEFAULT NULL
+  `totalSlots` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`idOptions`, `idEvent`, `duration`, `date`, `totalSlots`) VALUES
+(196, 41, 'PT15M', '2023-03-07 16:00:00', 2),
+(197, 41, 'PT15M', '2023-03-08 16:15:00', 1),
+(198, 41, 'PT15M', '2023-03-09 16:30:00', 1),
+(199, 41, 'PT15M', '2023-03-10 16:45:00', 0),
+(200, 41, 'PT15M', '2023-03-11 17:00:00', 0),
+(201, 41, 'PT15M', '2023-03-12 16:15:00', 0);
 
 -- --------------------------------------------------------
 
@@ -62,10 +83,20 @@ CREATE TABLE `options` (
 CREATE TABLE `reservations` (
   `idReservations` int(11) NOT NULL,
   `idOptions` int(11) DEFAULT NULL,
-  `idUsers` int(11) DEFAULT NULL,
-  `timeslot` timestamp NULL DEFAULT NULL,
-  `fileName` varchar(255) DEFAULT NULL
+  `hashUsers` varchar(8) DEFAULT NULL,
+  `origFileName` varchar(255) DEFAULT NULL,
+  `storedFileName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`idReservations`, `idOptions`, `hashUsers`, `origFileName`, `storedFileName`) VALUES
+(26, 196, '5c0f323e', NULL, NULL),
+(27, 197, '5c0f323e', NULL, NULL),
+(28, 198, '5c0f323e', NULL, NULL),
+(29, 196, '5c0f323e', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -78,8 +109,21 @@ CREATE TABLE `users` (
   `hashUsers` varchar(8) DEFAULT NULL,
   `onid` varchar(16) DEFAULT NULL,
   `lastName` varchar(255) DEFAULT NULL,
-  `firstName` varchar(255) DEFAULT NULL
+  `firstName` varchar(255) DEFAULT NULL,
+  `timeZone` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idUsers`, `hashUsers`, `onid`, `lastName`, `firstName`, `timeZone`) VALUES
+(20, '1d88b7b7', 'zhangxin2', 'Zhang', 'Xin', NULL),
+(21, 'abs213', 'test', 'John', 'Doe', NULL),
+(22, 'testId', 'test2', NULL, NULL, NULL),
+(23, 'testId3', 'test3', NULL, NULL, NULL),
+(24, 'testId4', 'test4', NULL, NULL, NULL),
+(25, '5c0f323e', 'test_onid', 'Doe', 'John', NULL);
 
 --
 -- Indexes for dumped tables
@@ -104,7 +148,7 @@ ALTER TABLE `options`
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`idReservations`),
   ADD KEY `idOptions` (`idOptions`),
-  ADD KEY `idUsers` (`idUsers`);
+  ADD KEY `idUsers` (`hashUsers`);
 
 --
 -- Indexes for table `users`
@@ -120,25 +164,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `idEvent` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEvent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `options`
 --
 ALTER TABLE `options`
-  MODIFY `idOptions` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idOptions` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `idReservations` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idReservations` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
@@ -154,8 +198,7 @@ ALTER TABLE `options`
 -- Constraints for table `reservations`
 --
 ALTER TABLE `reservations`
-  ADD CONSTRAINT `idOptions` FOREIGN KEY (`idOptions`) REFERENCES `options` (`idOptions`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `idUsers` FOREIGN KEY (`idUsers`) REFERENCES `users` (`idUsers`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `idOptions` FOREIGN KEY (`idOptions`) REFERENCES `options` (`idOptions`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
